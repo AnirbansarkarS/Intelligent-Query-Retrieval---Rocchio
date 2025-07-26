61710121a6ceff3704e25317ba96772a61e2de08
XpolioN <146839835+XpolioN2005@users.noreply.github.com>
2025-07-26 21:47:42 +0530

    updated things

--- main.py
+++ main.py
@@ -1,26 +1,10 @@
-# This is the main entry point of the project.
-# All core execution logic should be placed here.
-
-def main():
-	# Main application logic starts here
-	print("hello world")
-
-# Ensures this script only runs when executed directly,
-# preventing accidental execution from other file
-if __name__ == "__main__":
-	main()
-	from fastapi import FastAPI
+from fastapi import FastAPI
+from app.api import router
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello from FastAPI!"}
-
-
-from fastapi import FastAPI
-from app.api import router
-
-app = FastAPI()
 
 app.include_router(router)
