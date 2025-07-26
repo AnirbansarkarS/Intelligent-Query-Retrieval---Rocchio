a8eb3bed876d26820640fd3158c4ff034d5023c3
XpolioN <146839835+XpolioN2005@users.noreply.github.com>
2025-07-26 21:39:19 +0530

    edit main

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 # This is the main entry point of the project.
 # All core execution logic should be placed here.
 
-# TODO: Soumabha will handle the FastAPI logic separately.
-# If that code uses a different main file, we may need to merge it with this one.
-
 def main():
 	# Main application logic starts here
 	print("hello world")
