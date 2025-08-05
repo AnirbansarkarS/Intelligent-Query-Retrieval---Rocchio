0b866e509bd370fc773e6d61d686d09dbc2845b6
XpolioN <146839835+XpolioN2005@users.noreply.github.com>
2025-08-05 20:40:26 +0530

    Enhance file handling: update download_file to return filename and modify parse_document to handle file names for different content types

--- core/logic_evaluator.py
+++ core/logic_evaluator.py
@@ -1,18 +1,21 @@
 from typing import List
 from core.parser import parse_document
 from core.embbeding import run_pipeline
+from core.embbeding import file_exists
 from utils.output_answers import transform_answers
 
 import logging
 
+
 def evaluate_logic(document: str, questions: List[str]):
-    context= parse_document(document)
+    
+    id, context= parse_document(document)
 
     if not context:
         raise ValueError("Document parsing failed: context is empty.")
     try:
         pipeline_results = run_pipeline(
-            doc_id="policy_doc",
+            doc_id=ids,
             text=context,
             questions=questions,
             meta={"source": "policy.pdf"}
